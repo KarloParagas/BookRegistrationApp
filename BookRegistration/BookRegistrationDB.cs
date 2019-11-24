6e9723cc0f4b9ddad5c76279298c6bddee63a75b
KarloParagas <karloparagas@gmail.com>
2019-11-24 00:11:04 -0800

    Added method documentation

--- BookRegistration/BookRegistrationDB.cs
+++ BookRegistration/BookRegistrationDB.cs
@@ -9,9 +9,23 @@ namespace BookRegistration
 {
     static class BookRegistrationDB
     {
-        public static bool RegisterBook() 
+        /// <summary>
+        /// Registers a book and indicates if the operation was or was not successful
+        /// </summary>
+        /// <param name="regBook">Customer and book to be registered</param>
+        /// <returns></returns>
+        public static bool RegisterBook(Registration regBook) 
         {
-            throw new NotImplementedException();
+            if (Add(regBook) == true)
+            {
+                System.Windows.Forms.MessageBox.Show("The operation was successful");
+                return true;
+            }
+            else 
+            {
+                System.Windows.Forms.MessageBox.Show("The operation was NOT successful");
+                return false;
+            }
         }
 
         /// <summary>
@@ -19,7 +33,7 @@ namespace BookRegistration
         /// </summary>
         /// <exception cref="SqlException"></exception>
         /// <param name="r">Registration to be added</param>
-        public static void Add(Registration r) 
+        public static bool Add(Registration r) 
         {
             SqlConnection connection = DBHelper.GetConnection();
 
@@ -41,8 +55,13 @@ namespace BookRegistration
 
                 //Execute the query, and get the registration into the database
                 addCommand.ExecuteNonQuery();
+                return true;
             }
-            finally 
+            catch (SqlException) 
+            {
+                return false;
+            }
+            finally
             {
                 //Ensure the connection is closed and deletes the object
                 connection.Dispose();
