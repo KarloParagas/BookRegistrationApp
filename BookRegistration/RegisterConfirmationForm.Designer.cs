23b83e7ec6d569687fc30822765cf9c17f01d84e
KarloParagas <karloparagas@gmail.com>
2019-11-24 10:17:24 -0800

    Added a registration confirmation dialog box

---
+++ BookRegistration/RegisterConfirmationForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace BookRegistration
+{
+    partial class RegisterConfirmationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.yesBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Are you sure you want to register this book to the customer?";
+            // 
+            // yesBtn
+            // 
+            this.yesBtn.Location = new System.Drawing.Point(84, 72);
+            this.yesBtn.Name = "yesBtn";
+            this.yesBtn.Size = new System.Drawing.Size(75, 23);
+            this.yesBtn.TabIndex = 1;
+            this.yesBtn.Text = "Yes";
+            this.yesBtn.UseVisualStyleBackColor = true;
+            this.yesBtn.Click += new System.EventHandler(this.yesBtn_Click);
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Location = new System.Drawing.Point(186, 72);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.cancelBtn.TabIndex = 2;
+            this.cancelBtn.Text = "Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
+            // 
+            // RegisterConfirmationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 118);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.yesBtn);
+            this.Controls.Add(this.label1);
+            this.Name = "RegisterConfirmationForm";
+            this.Text = "Confirmation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button yesBtn;
+        private System.Windows.Forms.Button cancelBtn;
+    }
+}
---
+++ BookRegistration/RegisterConfirmationForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookRegistration
+{
+    public partial class RegisterConfirmationForm : Form
+    {
+        public RegisterConfirmationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void yesBtn_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+        }
+
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Nothing was registered");
+            Close();
+        }
+    }
+}
