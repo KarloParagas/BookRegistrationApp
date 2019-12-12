5d62fb65d9dffd4d8c185c5ffca0b7d19e05d9bd
KarloParagas <karloparagas@gmail.com>
2019-12-12 10:15:33 -0800

    Added a new deletion confirmation form for when a user wants to delete a customer or book.

---
+++ BookRegistration/DeleteConfirmationForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace BookRegistration
+{
+    partial class DeleteConfirmationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.YesBtn = new System.Windows.Forms.Button();
+            this.NoBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Are you sure?";
+            // 
+            // YesBtn
+            // 
+            this.YesBtn.Location = new System.Drawing.Point(27, 54);
+            this.YesBtn.Name = "YesBtn";
+            this.YesBtn.Size = new System.Drawing.Size(75, 23);
+            this.YesBtn.TabIndex = 1;
+            this.YesBtn.Text = "Yes";
+            this.YesBtn.UseVisualStyleBackColor = true;
+            // 
+            // NoBtn
+            // 
+            this.NoBtn.Location = new System.Drawing.Point(132, 54);
+            this.NoBtn.Name = "NoBtn";
+            this.NoBtn.Size = new System.Drawing.Size(75, 23);
+            this.NoBtn.TabIndex = 2;
+            this.NoBtn.Text = "No";
+            this.NoBtn.UseVisualStyleBackColor = true;
+            // 
+            // DeleteCustomerConfirmationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(240, 103);
+            this.Controls.Add(this.NoBtn);
+            this.Controls.Add(this.YesBtn);
+            this.Controls.Add(this.label1);
+            this.Name = "DeleteCustomerConfirmationForm";
+            this.Text = "Confirm Deletion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button YesBtn;
+        private System.Windows.Forms.Button NoBtn;
+    }
+}
